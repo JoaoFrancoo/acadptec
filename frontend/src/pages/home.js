3e4b74ebf574dd3073c337f5ac4a90077a0625be
Hermes <hermesalvesporto@gmail.com>
2024-10-18 13:30:59 +0100

    alinhamento video/banner pagina inicial(className edit)

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,13 +3,17 @@ import Img from '../imagens/img.png'
 
 function home() {
   return (
-    <div>
-      <div class="size-4/6"><video 
-      src={"https://cdn.pixabay.com/video/2023/10/15/185092-874643408_large.mp4"}
-      autoPlay
-      loop
-      muted
-       alt="imagem"></video> </div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="w-4/6" >
+        <video 
+          src={"https://cdn.pixabay.com/video/2023/10/15/185092-874643408_large.mp4"}
+          autoPlay
+          loop
+          muted
+          className='w-full h-auto'
+          alt="video"
+        ></video> 
+      </div>
       <div class="text-center text-3xl font-medium font-mono py-10">
        <h1>Bem vindo(a) à AcadPTec</h1>
        </div>
