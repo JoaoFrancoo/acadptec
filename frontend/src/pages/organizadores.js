8cc042c864b5198bd59863ec717b80c0a4c8bf60
Hermes <hermesalvesporto@gmail.com>
2024-10-24 01:49:54 +0100

    Adequação de convençaõ React, console.log inspecionando a resposta API,  verificando se data é um array antes de chamar map.

--- frontend/src/pages/organizadores.js
+++ frontend/src/pages/organizadores.js
@@ -1,35 +1,46 @@
 import React, {useEffect, useState} from 'react'
 
-function organizadores() {
-    const[data,setData]=useState([])
+function Organizadores() {
+    const[data,setData]=useState([]);
     useEffect(()=>{
       fetch('http://localhost:8081/users')
       .then(res => res.json())
-      .then(data => setData(data))
+      .then(data => {
+        console.log(data); //Verifique o formato da resposta
+        setData(data);
+      })
       .catch(err => console.log(err));
-    },[])
+    },[]);
     return (
       <div style={{padding: "50px"}}>
           <table>
             <thead>
-              <th>ID</th>
-              <th>nome</th>
-              <th>email</th>
-              <th>departamento</th>
+              <tr>
+                <th>ID</th>
+                <th>nome</th>
+                <th>email</th>
+                <th>departamento</th>
+              </tr>          
             </thead>
             <tbody>
-              {data.map((d, i) =>(
+              {Array.isArray(data) ? (
+              data.map((d, i) =>(
                 <tr key={i}>
                   <td>{d.id_organizador}</td>
                   <td>{d.nome}</td>
                   <td>{d.email}</td>
                   <td>{d.departamento}</td>
                 </tr>
-              ))}
+              ))
+            ):(
+              <tr>
+                <td colSpan="4">Dados não disponíveis.</td>
+              </tr>
+            )}
             </tbody>
           </table>
       </div>
-    )
+    );
 }
 
-export default organizadores
+export default Organizadores;
